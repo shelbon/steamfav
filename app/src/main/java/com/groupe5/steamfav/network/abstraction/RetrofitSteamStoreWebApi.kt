b0a911a6cfc0238e1b0515c6a3b27e467a34d0eb
shelbon <mouclepatrick@protonmail.com>
2023-01-15 18:55:41 +0100

    feat: adapt take to user device language

--- app/src/main/java/com/groupe5/steamfav/network/abstraction/RetrofitSteamStoreWebApi.kt
+++ app/src/main/java/com/groupe5/steamfav/network/abstraction/RetrofitSteamStoreWebApi.kt
@@ -4,10 +4,11 @@ import com.groupe5.steamfav.network.models.GameDetails
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.util.*
 
 interface RetrofitSteamStoreWebApi {
     @GET("/api/appdetails")
-    suspend fun getGameDetails(@Query("appids") gameId: Long): Response<GameDetails>
+    suspend fun getGameDetails(@Query("appids") gameId: Long,@Query("l") language:String= Locale.getDefault().getDisplayLanguage(Locale.ENGLISH).lowercase()): Response<GameDetails>
 
 
 }
