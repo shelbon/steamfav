39902398f6f4703f48274f756864690b14db4569
shelbon <mouclepatrick@protonmail.com>
2023-01-31 12:38:48 +0100

    feat: use dependency injection with Koin

--- app/src/main/java/com/groupe5/steamfav/ui/fragments/GameDetailsFragment.kt
+++ app/src/main/java/com/groupe5/steamfav/ui/fragments/GameDetailsFragment.kt
@@ -5,31 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.groupe5.steamfav.R
-import com.groupe5.steamfav.data.GamesRepository
 import com.groupe5.steamfav.databinding.FragmentGameDetailsBinding
-import com.groupe5.steamfav.network.services.SteamStoreNetwork
-import com.groupe5.steamfav.network.services.SteamWorksWebNetwork
 import com.groupe5.steamfav.viewmodels.GameDetailsViewModel
-import com.groupe5.steamfav.viewmodels.factory.ViewModelFactory
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class GameDetails : Fragment() {
 
 
-    private val viewModel: GameDetailsViewModel by viewModels {
-        ViewModelFactory(
-            this,
-            GamesRepository(
-                SteamWorksWebNetwork(),
-                SteamStoreNetwork()
-            )
-        )
-    }
+    private val viewModel: GameDetailsViewModel by viewModel()
     private val args by navArgs<GameDetailsArgs>()
     private var _binding: FragmentGameDetailsBinding? = null
     private val binding get() = _binding!!
@@ -41,7 +29,7 @@ class GameDetails : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentGameDetailsBinding.inflate(inflater, container, false)
         binding.gameDetailsText.text = args.gameId.toString()
         appBarConfiguration = AppBarConfiguration(setOf(R.navigation.search,R.navigation.main))
